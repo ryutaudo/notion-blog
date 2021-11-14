5a659f34f67ecdba4872fac92b481a8df91ceca5
有働龍太 <ryutaudo@gmail.com>
2021-11-14 22:08:40 +0900

    Customize my social media links

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,22 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/ryuta_udo',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
+    link: 'https://github.com/ryutaudo',
   },
   {
     Comp: LinkedIn,
     alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+    link: 'https://www.linkedin.com/in/ryutaudo/',
   },
   {
     Comp: Envelope,
     alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'ryutaudo@gmail.com?subject=Notion Blog',
   },
 ]
 
@@ -44,8 +44,8 @@ export default function Contact() {
         <h1 style={{ marginTop: 0 }}>Contact</h1>
 
         <div className={contactStyles.name}>
-          JJ Kasper - Next.js Engineer @{' '}
-          <ExtLink href="https://vercel.com">Vercel</ExtLink>
+          Ryuta Udo - Frontend Developer @{' '}
+          <ExtLink href="https://www.milleporte.com/">Milleporte</ExtLink>
         </div>
 
         <div className={contactStyles.links}>
